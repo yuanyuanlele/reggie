c5ed089d39c466facf5fbd6b3f8bb084fff370f0
yuanyuanlele <1468817353@qq.com>
2023-04-24 20:35:06 +0800

    1

---
+++ src/main/java/com/le/reggie/controller/CommonController.java
@@ -0,0 +1,24 @@
+package com.le.reggie.controller;
+
+import com.le.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@Slf4j
+@RequestMapping("/common")
+public class CommonController {
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file){
+        log.info(file.toString());
+        return null;
+    }
+}
