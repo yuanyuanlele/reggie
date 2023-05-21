b0e65dc24a34bb2e83f5ab5543629f58c33229e1
yuanyuanlele <1468817353@qq.com>
2023-05-21 21:42:51 +0800

    redis完成

--- src/main/java/com/le/reggie/controller/SetmealController.java
+++ src/main/java/com/le/reggie/controller/SetmealController.java
@@ -14,6 +14,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -43,6 +45,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value ="setmealCache" ,allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("套餐信息：{}",setmealDto);
 
@@ -58,6 +61,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping("/status/{status}")
+    @CacheEvict(value ="setmealCache" ,allEntries = true)
     public R<String> updateStatus(@PathVariable("status") int status,@RequestParam List<Long> ids){
         setmealService.updateStatus(status,ids);
         return R.success("更新状态成功");
@@ -114,6 +118,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value ="setmealCache" ,allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
         log.info("ids:{}",ids);
 
@@ -155,6 +160,7 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId+'_'+#setmeal.status")
     public R<List<Setmeal>> list(Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(setmeal.getCategoryId() != null,Setmeal::getCategoryId,setmeal.getCategoryId());
